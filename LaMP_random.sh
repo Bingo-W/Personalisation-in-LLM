9dd7832f04022eb06e8c80be19b9b5996ff7d98e
Bin <wubin33@mail2.sysu.edu.cn>
2023-11-28 10:51:06 +0000

    Added some required environments.

--- LaMP_random.sh
+++ LaMP_random.sh
@@ -1,32 +1,34 @@
 export CUDA_VISIBLE_DEVICES="1"
 
+RETRIEVAL_ID='Full_Random'
+
 python train.py \
     --task_name 'LaMP_1' \
     --task_pattern 'user-based' \
-    --retrieval_id 'Random' \
+    --retrieval_id ${RETRIEVAL_ID} \
     --training_epoch '10'
 
 python train.py \
     --task_name 'LaMP_2' \
     --task_pattern 'user-based' \
-    --retrieval_id 'Random' \
+    --retrieval_id ${RETRIEVAL_ID} \
     --training_epoch '10'
 
 python train.py \
     --task_name 'LaMP_4' \
     --task_pattern 'user-based' \
-    --retrieval_id 'Random' \
+    --retrieval_id ${RETRIEVAL_ID} \
     --training_epoch '20'
 
 python train.py \
     --task_name 'LaMP_5' \
     --task_pattern 'user-based' \
-    --retrieval_id 'Random' \
+    --retrieval_id ${RETRIEVAL_ID} \
     --training_epoch '20'
 
 python train.py \
     --task_name 'LaMP_7' \
     --task_pattern 'user-based' \
-    --retrieval_id 'Random' \
+    --retrieval_id ${RETRIEVAL_ID} \
     --training_epoch '20'
 
