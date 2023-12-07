55f04938128ae5662f4411f04bf920aaf814ecb5
Bin <wubin33@mail2.sysu.edu.cn>
2023-12-07 18:01:31 +0000

    Fixed the bugs for the invalied output for LaMP_3

--- metrics/lamp_metrics.py
+++ metrics/lamp_metrics.py
@@ -36,8 +36,15 @@ def text_F1(preds:list, labels:list):
 def text_MAE(preds:list, labels:list):
     assert (len(preds) == len(labels))
 
-    transform_preds = [int(item) if len(item)==1 else int(item)[:-4] for item in preds]
     transform_labels = [int(item) if len(item)==1 else int(item)[:-4] for item in labels]
+
+    transform_preds = []
+    for index_, item in enumerate(preds):
+        try:
+            transform_preds.append(int(item))
+        except:
+            transform_preds.append(transform_labels[index_]-5)
+    
 
     mae_metric = evaluate.load("mae")
     results = mae_metric.compute(predictions=transform_preds, references=transform_labels)
@@ -47,8 +54,14 @@ def text_MAE(preds:list, labels:list):
 def text_RMSE(preds:list, labels:list):
     assert (len(preds) == len(labels))
 
-    transform_preds = [int(item) if len(item)==1 else int(item)[:-4] for item in preds]
     transform_labels = [int(item) if len(item)==1 else int(item)[:-4] for item in labels]
+
+    transform_preds = []
+    for index_, item in enumerate(preds):
+        try:
+            transform_preds.append(int(item))
+        except:
+            transform_preds.append(transform_labels[index_]-5)
 
     mae_metric = evaluate.load("mse")
     results = mae_metric.compute(predictions=transform_preds, references=transform_labels)
