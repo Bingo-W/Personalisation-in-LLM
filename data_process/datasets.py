48f82c8ca319704f6581855856f4ef91ff0a0a7b
Bin <wubin33@mail2.sysu.edu.cn>
2023-12-18 17:03:24 +0000

    Fixed the bugs for the Mixed,

--- data_process/datasets.py
+++ data_process/datasets.py
@@ -153,7 +153,7 @@ class MyDatasets():
                         profiles_for_input = self.__sample_among_users(batch_size*self._retrieval_num, retrieval_fn['input'])
                     else:
                         profiles_for_input = [
-                        retrieval_fn['output'](task_input, user_profile, self._retrieval_num) \
+                        retrieval_fn['input'](task_input, user_profile, self._retrieval_num) \
                         for task_input, user_profile in zip(sample['input'], sample['profile'])
                     ]
                     if self._output_retrieval_id == 'Full_Random':
